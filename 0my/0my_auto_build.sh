bc53079720c435bbdf059dfcd688416eeef6e636
jundeng(邓军) <jundeng@futunn.com>
2019-09-27 14:14:25 +0800

    up

--- 0my/0my_auto_build.sh
+++ 0my/0my_auto_build.sh
@@ -5,11 +5,8 @@ CURDIR=$(pwd)
 PROJECT_ROOT=$(dirname $0)/..
 cd $PROJECT_ROOT
 
-git remote add upstream https://github.com/h2y/Shadowrocket-ADBlock-Rules.git
 git fetch --all
-git checkout master
 git rebase upstream/master origin/master
-git push origin master
 
 cd $CURDIR
 /usr/bin/env python3 0my_build_confs.py
